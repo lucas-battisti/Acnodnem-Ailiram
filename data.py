34cd9d5a32d12dc87664a80e60cb083be3107018
Lucas Battisti <lucas.sala.battisti.pessoal@gmail.com>
2024-05-13 00:50:16 -0300

    extra + notebooks

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ and a custom LightningDataModule class.
 """
 
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
 
 from typing import Tuple
 
@@ -34,28 +34,35 @@ def tabular_norm(tensor: torch.Tensor) -> torch.Tensor:
 
 class FF_Dataset(Dataset):
     def __init__(self, xez: Tuple[pd.core.frame.DataFrame],
-                 super_am=False, p=1,
-                 norm=False):
+                 extra: pd.core.frame.DataFrame = None,
+                 p=1, norm=False):
         
         self.z = torch.tensor(xez[2].values)
         
-        if super_am:
+        if xez[1] is None:
             x = torch.tensor(xez[0].fillna(0).values)
-            e = torch.tensor(xez[1].fillna(0).values)
-            x = self.f.repeat(p, 1)
-            e = self.e.repeat(p, 1)
-            self.z = self.z.repeat(p, 1)
-            self.covariables = torch.normal(x, e)
-        else:
-            if xez[1] is None:
-                self.covariables = torch.tensor(xez[0].fillna(0).values)
-            else:
-                self.covariables = torch.cat((torch.tensor(xez[0].fillna(0).values),
-                                  torch.tensor(xez[1].fillna(0).values)), dim=1)
             
-        if norm:
-            self.covariables = tabular_norm(self.covariables)
-
+            e = torch.zeros(x.size()[0], x.size()[1])
+            
+        x = torch.tensor(xez[0].fillna(0).values)
+        e = torch.tensor(xez[1].fillna(0).values)
+        x = self.f.repeat(p, 1)
+        e = self.e.repeat(p, 1)
+        
+        self.z = self.z.repeat(p, 1)
+            
+        if p == 1:
+            if xez[1]:
+                self.covariables = torch.cat((x, e), dim=1)
+            else:
+                self.covariables = x
+        else:
+            self.covariables = torch.normal(x, e)
+            
+        if extra:
+            extra = torch.tensor(extra).repeat(p, 1)
+            self.covariables = torch.cat((self.covariables, extra), dim=1)
+            
     def __getitem__(self, idx):
         return self.covariables[idx], self.z[idx]
 
@@ -90,29 +97,36 @@ def matrix_1d_norm(tensor: torch.Tensor) -> torch.Tensor:
     
 class CNN1D_Dataset(Dataset):
     def __init__(self, xez: Tuple[pd.core.frame.DataFrame],
+                 extra: pd.core.frame.DataFrame = None,
                  version=['no', 'with', 'stack'],
-                 super_am=False, p=1,
-                 norm=False):
+                 p=1, norm=False):
 
-        if super_am and version != 'no':
+        if p != 1 and version != 'no':
             raise ValueError("super and not 'no'")
 
         self.z = torch.tensor(xez[2].values)
 
         x = torch.tensor(xez[0].fillna(0).values)
         e = torch.tensor(xez[1].fillna(0).values)
-
-        if super_am:
-            x = x.repeat(p, 1)
-            e = e.repeat(p, 1)
-            self.z = self.z.repeat(p, 1)
+        x = x.repeat(p, 1)
+        e = e.repeat(p, 1)
+        
+        self.z = self.z.repeat(p, 1)
+        
+        if p == 1:
+            self._1d = make_vectors(x, e, version=version)
+        else:
             new_x = torch.normal(x, e)
             self._1d = make_vectors(new_x, e, version='no')
-        else:
-            self._1d = make_vectors(x, e, version=version)
-            
+        
         if norm:
             self._1d = matrix_1d_norm(self._1d)
+            
+        if extra:
+            extra = torch.tensor(extra)
+            extra = torch.reshape(extra, (len(extra), 1, extra.size()[1]))
+            extra = extra.repeat(p, x.size()[1], 1)
+            self._1d = torch.cat((self._1d, extra), dim=2)
 
     def __getitem__(self, idx):
         return self._1d[idx], self.z[idx]
@@ -148,26 +162,87 @@ def make_matrices(
     normal = torch.distributions.normal.Normal(loc=mu_p, scale=e)
     
     return normal.log_prob(x).exp()
-    
-class CNN2D_Dataset(Dataset): #add norm
-    def __init__(self, xez: Tuple[pd.core.frame.DataFrame],
-                 K: int, t_inf: float, t_sup: float,
-                 norm=False):
 
+def matrix_2d_norm(tensor: torch.Tensor) -> torch.Tensor:
+    sigma, mu = torch.std_mean(tensor, dim=(0, 2, 3))
+    
+    n_channels = len(tensor[0])
+    
+    for i in range(n_channels):
+        tensor[:, i, :, :] = (tensor[:, i, :, :] - mu[i])/sigma[i]
+    return tensor
+    
+class CNN2D_Dataset(Dataset): 
+    def __init__(self, xez: Tuple[pd.core.frame.DataFrame],
+                 extra: pd.core.frame.DataFrame = None,
+                 K: int=None, t_inf: float=None, t_sup: float=None,
+                 norm=False):
+        
         self.z = torch.tensor(xez[2].values)
 
-        x = torch.tensor(xez[0].values)
-        e = torch.tensor(xez[1].values)
-
+        x = torch.tensor(xez[0].fillna(0).values)
+        e = torch.tensor(xez[1].fillna(0).values)
+        
         self._2d = make_matrices(x, e, K, t_inf, t_sup)
+
+        if norm:
+            self._2d = matrix_2d_norm(self._2d)
+            
+        if extra:
+            extra = torch.tensor(extra)
+            extra = torch.reshape(extra, (len(extra), 1, 1, extra.size()[1]))
+            extra = extra.repeat(1, x.size()[1], x.size()[3], 1)
+            self._2d = torch.cat((self._2d, extra), dim=3)
 
     def __getitem__(self, idx):
         return self._2d[idx], self.z[idx]
 
     def __len__(self):
         return len(self.z)
+    
+class Custom_DataModule(L.LightningDataModule):
+    def __init__(self, *args, xez: Tuple[pd.core.frame.DataFrame],
+                 extra: pd.core.frame.DataFrame = None,
+                 dataset_class,
+                 split_size_or_sections=[0.5, 0.25, 0.25], seed: int=2023,
+                 batch_size:int = None, num_workers: int=None,
+                 **kwargs):
+        super().__init__(*args)
+        
+        data = dataset_class((xez[0], xez[1], xez[2]), extra, **kwargs)
+        
+        n = len(data)
+        
+        shuffled_ind = torch.randperm(n, generator=torch.manual_seed(seed))
+        
+        if len(split_size_or_sections) == 2:
+            ind_train, ind_val = torch.split(shuffled_ind, split_size_or_sections)
+            ind_test = ind_val
+            
+        if len(split_size_or_sections) == 3:
+            ind_train, ind_val, ind_test = torch.split(shuffled_ind, split_size_or_sections)
+        
+        self.train = Subset(data, ind_train)
+        self.val = Subset(data, ind_val)
+        self.test = Subset(data, ind_test)
+        
+        self.batch_size = batch_size
+        self.num_workers = num_workers
 
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
+    def val_dataloader(self):
+        return DataLoader(self.val, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers)
+
+        
+
+##################################
+
+'''
 class Custom_DataModule(L.LightningDataModule):
     def __init__(self, *args, xez: Tuple[pd.core.frame.DataFrame],
                  dataset_class,
@@ -220,3 +295,4 @@ class Custom_DataModule(L.LightningDataModule):
 
         
 
+'''
